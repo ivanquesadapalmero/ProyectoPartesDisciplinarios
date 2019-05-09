1a32d0167b9c83cce0c0c4bd1254a98f1fb43662
Iván Quesada <32323558+ivanquesadapalmero@users.noreply.github.com>
2019-05-09 08:25:38 +0200

    Update alumnos.sql

--- ScriptsBD/alumnos.sql
+++ ScriptsBD/alumnos.sql
@@ -1,1 +1,20 @@
-
+INSERT INTO alumnos VALUES(1,'Iván','Quesada Palmero','2ºDAW');
+INSERT INTO alumnos VALUES(2,'David','Morano Pérez','2ºDAW');
+INSERT INTO alumnos VALUES(3,'Jesus','Delgado Marín','2ºDAW');
+INSERT INTO alumnos VALUES(4,'Sergio','Pérez Rueda','2ºDAW');
+INSERT INTO alumnos VALUES(5,'José Antonio','Marquez Pérez','2ºDAW');
+INSERT INTO alumnos VALUES(6,'Amelia','Paniagua Gálvez','2ºDAW');
+INSERT INTO alumnos VALUES(7,'Jesus Diego','Cruz Diaz','2ºDAW');
+INSERT INTO alumnos VALUES(8,'Manuel Jesús','Martín Prieto','2ºDAW');
+INSERT INTO alumnos VALUES(9,'Juan de Dios','Bermudo Delgado','2ºDAW');
+INSERT INTO alumnos VALUES(10,'Miguel','Sanchez Torrijo','2ºDAW');
+INSERT INTO alumnos VALUES(11,'Ramón Jesus','Gomez Carmona','1ºASIR');
+INSERT INTO alumnos VALUES(12,'Daniel','Atenciano Ródriguez','1ºASIR');
+INSERT INTO alumnos VALUES(13,'Rafael','Álvarez-Ossorio Martín','1ºASIR');
+INSERT INTO alumnos VALUES(14,'Juan Alberto','Muñiz Fernandez','1ºDAW');
+INSERT INTO alumnos VALUES(15,'Jose Manuel','Sanchez Olivar','1ºDAW');
+INSERT INTO alumnos VALUES(16,'Isabel','Bermudo Lopéz','1ºDAW');
+INSERT INTO alumnos VALUES(17,'María','Martín Pérez','2ºASIR');
+INSERT INTO alumnos VALUES(18,'Pablo','Romero Hidalgo','2ºASIR');
+INSERT INTO alumnos VALUES(19,'Luis','Vidal Montilla','2ºASIR');
+INSERT INTO alumnos VALUES(20,'Daniela','Sanchez Castillo','2ºASIR');
