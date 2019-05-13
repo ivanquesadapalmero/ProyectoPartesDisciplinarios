8575a6c62f8b399c6ff0efc1867460fce1383e28
ivanquesada <ivanquesadapalmero@gmail.com>
2019-05-13 14:20:14 +0200

    prueba

--- inicio.php
+++ inicio.php
@@ -21,11 +21,11 @@
 	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
 	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
 </head>
-
 <body>
 <?php 
 	include('utilidades/session.php'); 
 ?>
+
 	<!-- WRAPPER -->
 	<div id="wrapper">
 		<!-- NAVBAR -->
