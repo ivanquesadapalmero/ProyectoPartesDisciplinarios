a6205d5d026ad6796fd45f5c27e4f87b03604d52
Iván Quesada <32323558+ivanquesadapalmero@users.noreply.github.com>
2019-05-21 12:03:19 +0200

    Update bd.php

--- utilidades/bd.php
+++ utilidades/bd.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_SERVER', 'den1.mysql2.gear.host');
+define('DB_USERNAME', 'gestiondepartes');
+define('DB_PASSWORD', 'Kx1z8x?0Up~s	');
 define('DB_DATABASE', 'gestiondepartes');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die("No se ha podido establecer la conxión con la base de datos");
 
